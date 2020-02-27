59c8c8d79b5c341229e37eac7e885b054fd4893d
kevin.diaz <kevin.diaz@CLQL001241>
2020-02-27 12:46:33 -0600

    Basic annotations

--- metrics/src/main/java/com/metrics/config/WebConfig.java
+++ metrics/src/main/java/com/metrics/config/WebConfig.java
@@ -1,5 +1,17 @@
 package com.metrics.config;
 
-public class WebConfig {
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
+	}
 
 }
--- metrics/src/main/java/com/metrics/service/MetricsServiceImpl.java
+++ metrics/src/main/java/com/metrics/service/MetricsServiceImpl.java
@@ -1,5 +1,8 @@
 package com.metrics.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class MetricsServiceImpl {
 
 }
