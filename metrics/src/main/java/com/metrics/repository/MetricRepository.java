30301ae750f4140ced20ef9d9d10eda259631180
kevin.diaz <kevin.diaz@CLQL001241>
2020-03-09 11:12:52 -0600

    Delete Method Repository

--- metrics/src/main/java/com/metrics/repository/MetricRepository.java
+++ metrics/src/main/java/com/metrics/repository/MetricRepository.java
@@ -1,5 +1,9 @@
 package com.metrics.repository;
 
-public interface MetricRepository {
+import org.springframework.data.mongodb.repository.MongoRepository;
 
+import com.metrics.model.MetricsCollection;
+
+public interface MetricRepository extends MongoRepository<MetricsCollection, String>{
+	void deleteByID(String id);	
 }
