0bbad9a954fc5de1064f6caa05436b946d565f22
kdiaz13 <61017014+kdiaz13@users.noreply.github.com>
2020-03-17 12:10:14 -0600

    Delete Unit Test

--- src/test/java/com/metrics/repository/ControllerTest.java
+++ src/test/java/com/metrics/repository/ControllerTest.java
@@ -50,87 +50,11 @@ class MetricRepositoryTest {
        }
 
  
-
-    
-    @Test
-    public void test_update_user_success() throws Exception {
-        CreateMetricRequest metric = newCreateMetricRequest();
-        
-        
-
- 
-
-        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.
-                put("/metrics/{id}", metric.getId())
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .content(mapToJson(metric)))
-                .andExpect(handler().handlerType(MetricsController.class))
-                .andExpect(handler().methodName("updateMetric"))
-                .andDo(print())
-                .andReturn();
-        assertEquals(202, mvcResult.getResponse().getStatus());
-        String jsonResponse = mvcResult.getResponse().getContentAsString();
-        assertEquals(jsonResponse, mapToJson(metric));
-    }
-
- 
-
-    
-    
-    @Test
-    public void test_update_user_fail_404_not_found() throws Exception {
-        CreateMetricRequest falseMetric = falseCreateMetricRequest();
-
- 
-
-        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.
-                put("/metrics/{id}", falseMetric.getId())
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .content(mapToJson(falseMetric)))
-                .andExpect(handler().handlerType(MetricsController.class))
-                .andExpect(handler().methodName("updateMetric"))
-                .andDo(print())
-                .andReturn();
-        assertEquals(404, mvcResult.getResponse().getStatus());
-        String jsonResponse = mvcResult.getResponse().getContentAsString();
-        assertEquals(jsonResponse, "");
-
-        
-    }
-
-	@Test
-	public void testPOSTMetric() throws Exception
-	{
-		
-		CreateMetricRequest metric = newCreateMetricRequest();
-        
-        MvcResult result = mvc.perform(
-        					MockMvcRequestBuilders.post("/metrics")
-        						.contentType(MediaType.APPLICATION_JSON)
-        						.accept(MediaType.APPLICATION_JSON).content(mapToJson(metric)))
-        						.andReturn();            
-       
-        assertEquals(201, result.getResponse().getStatus());
-        
-	}
-    
-    
-    private CreateMetricRequest newCreateMetricRequest () {
-        return new CreateMetricRequest("5e691fd8cdcafe60f029b807","Empty","Empty","Empty","1001-01-02","Empty",new metrics(false,false,
-                                    new blockers(false,"Empty"),
-                                    new proactive(false, false,false,false),
-                                    new retroactive(false,"Empty")));
-    }
-    
-    private CreateMetricRequest falseCreateMetricRequest () {
-        return new CreateMetricRequest("5e691fd8cdc","Empty","Empty","Empty","1001-01-02","Empty",new metrics(false,false,
-                                    new blockers(false,"Empty"),
-                                    new proactive(false, false,false,false),
-                                    new retroactive(false,"Empty")));
-    }
-    
-    protected String mapToJson(Object obj) throws JsonProcessingException {
-          ObjectMapper objectMapper = new ObjectMapper();
-          return objectMapper.writeValueAsString(obj);
-       }
+        @Test
+ 	   public void deleteMetric() throws Exception {
+ 	      String uri = "/metrics/";
+ 	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete(uri)).andReturn();
+ 	      int status = mvcResult.getResponse().getStatus();
+ 	      assertEquals(204, status);
+ 	   }
 }
