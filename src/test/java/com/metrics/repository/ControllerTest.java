fce056e7c32ac088b5e8994f8d0384326744554f
kdiaz13 <61017014+kdiaz13@users.noreply.github.com>
2020-03-18 09:05:32 -0600

    Unit Test Completed

--- src/test/java/com/metrics/repository/ControllerTest.java
+++ src/test/java/com/metrics/repository/ControllerTest.java
@@ -11,7 +11,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -21,17 +20,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
  
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import com.metrics.MetricsApplication;
-import com.metrics.controller.MetricsController;
-import com.metrics.domain.CreateMetricRequest;
-import com.metrics.model.MetricsCollection;
-import com.metrics.model.blockers;
-import com.metrics.model.metrics;
-import com.metrics.model.proactive;
-import com.metrics.model.retroactive;
-import com.metrics.service.MetricsServiceImpl;
 
  
 
@@ -51,10 +41,18 @@ class MetricRepositoryTest {
 
  
         @Test
- 	   public void deleteMetric() throws Exception {
- 	      String uri = "/metrics/";
+ 	   public void deleteMetricCorrect() throws Exception {
+ 	      String uri = "/metrics/424y5y5y56y";
  	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete(uri)).andReturn();
  	      int status = mvcResult.getResponse().getStatus();
  	      assertEquals(204, status);
  	   }
+        
+        @Test
+  	   public void deleteMetricBadID() throws Exception {
+  	      String uri = "/metrics/t54t5yt5";
+  	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete(uri)).andReturn();
+  	      int status = mvcResult.getResponse().getStatus();
+  	      assertEquals(404, status);
+  	   }
 }
